f0c743a28f6740c818ace25a8241b6018f7dd1d2
Maester-Khris <ndevjoel@gmail.com>
2025-06-14 00:43:18 -0400

    model building and training: done

--- old_pipeline.py
+++ old_pipeline.py
@@ -1,8 +1,10 @@
 import spacy_sentence_bert
 from sklearn.model_selection import train_test_split
 
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
 from sklearn.metrics import accuracy_score
 
 
